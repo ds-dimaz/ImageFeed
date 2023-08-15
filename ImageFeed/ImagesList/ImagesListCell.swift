3e0d7deb013bcac59054408b3db937940cb4c92e
Dmitrii <dimaz.pense@gmail.com>
2023-08-15 17:44:07 +0400

    Added TableView and mock images

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,29 @@
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var gradientView: UIView!
+    
+    private var gradientInited = false
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    func configureCell(for cell: ImagesListCell) {
+        if !gradientInited {
+            let gradient = CAGradientLayer()
+            gradient.frame = gradientView.bounds
+            gradient.colors = [UIColor.clear.cgColor, UIColor.ypBlack.withAlphaComponent(0.5).cgColor]
+            gradientView.layer.insertSublayer(gradient, at: 0)
+            let path = UIBezierPath(roundedRect: gradientView.bounds, byRoundingCorners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = path.cgPath
+            gradientView.layer.mask = maskLayer
+
+            gradientInited.toggle()
+        }
+    }
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,62 @@
+import Foundation
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "LikeButtonOn") : UIImage(named: "LikeButtonOff")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("Unable to dequeue a ImagesListCell from the tableView. Returning a UITableViewCell instead.")
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        imageListCell.configureCell(for: imageListCell)
+        
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    }
+}
+
