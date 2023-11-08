b81cb3d16809a3373ac559c11b55e16484ba2be7
Dmitrii <dimaz.pense@gmail.com>
2023-11-08 12:37:54 +0400

    Created and added Web View 

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey = "rNAMRmpZtWtl64suHtv0girPypYlKXQYkQiqmqJAGHE"
+let secretKey = "EexLue6-HuJdJM2xsI_OOSdRcwWlIzZDFVEaQe9idiQ"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
